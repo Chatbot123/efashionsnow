cf6dca022d0f11e05b6563e6afcf9e39c06cc611
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-03-15 14:30:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -324,7 +324,7 @@ if($method == 'POST')
 		$jsonoutput = json_decode($response);
 		//echo $jsonoutput;
 		$AuthToken =  $jsonoutput->result;
-		//$speech = "Your Auth number is ".$AuthToken;
+		$speech = "Your Auth number is ".$AuthToken;
 		
 		//Get release key of process
 		$query = "https://platform.uipath.com/odata/Releases";
@@ -342,7 +342,7 @@ if($method == 'POST')
 		curl_close($curl);
 		$jsonoutput = json_decode($response);
 		$ReleaseKey =  $jsonoutput->value[0]->Key;
-		//$speech = "Your Auth number is ".$ReleaseKey;
+		$speech .= " Your release key is ".$ReleaseKey;
 		
 		//GET ROBOT ID
 		//https://platform.uipath.com/odata/Robots?$top=1&$filter=Name eq 'guessnum'
@@ -360,7 +360,7 @@ if($method == 'POST')
 		curl_close($curl);
 		$jsonoutput = json_decode($response);
 		$RobotId =  $jsonoutput->value[0]->Id;
-		
+		$speech .= "  ".$query."  ".$RobotId;
 		//START A JOB
 		$query = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs";
 		$curl = curl_init($query);
@@ -398,7 +398,7 @@ if($method == 'POST')
 		curl_close($curl);
 		//$jsonoutput = json_decode($response);
 		//echo $jsonoutput;
-		$speech = 'Starting the game...';
+		$speech .= ' Starting the game...';
 		
 		
 		
