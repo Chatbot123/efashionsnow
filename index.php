2385e16815a5d2dce857a8bac6522a97877c3401
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-03-19 10:54:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -513,7 +513,7 @@ if($method == 'POST')
 		
 		
 		//START A JOB
-		$geo_country=$json->queryResult->parameters->geo-country;
+		$geo_country=$json->queryResult->parameters->country;
 		echo $geo_country;
 		$query = "https://platform.uipath.com/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs";
 		$curl = curl_init($query);
