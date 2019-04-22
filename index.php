fb60b1dbb4dd865b3ec2fd357a79fb4abf4f5b82
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-04-22 12:27:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -396,7 +396,7 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "PUT",
   CURLOPT_POSTFIELDS => $jsonvar,
-  CURLOPT_HTTPHEADER => array(  $xcsrftoken  ),
+  CURLOPT_HTTPHEADER => array(  "x-csrf-token: M6CeRAEJCeDmEnupNLl9GQ=="  ),
 ));
 
 $response = curl_exec($curl);
