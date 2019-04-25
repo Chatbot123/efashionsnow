67ba191091e165ff1d3eaed6ff57f7d59065989e
Chatbot123 <42810864+Chatbot123@users.noreply.github.com>
2019-04-25 15:10:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -766,6 +766,7 @@ $body = substr($response, $header_size);
 header("Content-Type:application/json");
 $headers = explode("\r\n", $headers); // The seperator used in the Response Header is CRLF (Aka. \r\n) 
 $headers = array_filter($headers);
+echo $headers;
 //extracting status from header
 $httpstatus = $headers[0];
 echo $httpstatus;
